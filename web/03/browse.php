f4b3864d3ae2612ddf8f1caf4089a85af2d42d09
Matthew Burr <matt.d.burr@gmail.com>
2018-01-22 22:13:44 -0600

    Added basic content for Week 03 assignment

---
+++ web/03/browse.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+  <head>
+    <?php require 'header_include.php' ?>
+    <title>Shopping Cart</title>
+  </head>
+  <body>
+    <?php require 'navigation_include.php' ?>
+    <div class="container">
+       <div class="row">
+        <div class="col-sm">
+          <h1 class="mt-2">Catalog</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm border boder-secondary">todo: create content</div>
+      </div>
+    </div>
+    <?php require 'script_include.php' ?>
+  </body>
+</html>
---
+++ web/03/navigation_include.php
@@ -0,0 +1,3 @@
+<nav class="navbar navbar-expand-sm bg-secondary navbar-dark">
+  <a href="../index.php" class="navbar-brand">MB</a>
+</nav>
