fccadb71880c2219a75f2050a0f09ccea3a79fea
Matthew Burr <matt.d.burr@gmail.com>
2018-01-27 08:29:36 -0600

    Added items and buttons

--- web/03/browse.php
+++ web/03/browse.php
@@ -13,7 +13,49 @@
         </div>
       </div>
       <div class="row">
-        <div class="col-sm border boder-secondary">todo: create content</div>
+        <div class="col-sm">
+          <table class="table">
+            <thead class="thead-dark">
+              <tr>
+                <th scope="col">Item</th>
+                <th scope="col">Description</th>
+                <th scope="col">&nbsp;</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Giant Widget</td>
+                <td>A widget for your biggest machines.</td>
+                <td class="text-right"><input type="button" value="Add to cart" class="btn btn-primary add-to-cart-btn" id="giant-widget" /></td>
+              </tr>
+              <tr>
+                <td>Large Widget</td>
+                <td>A widget for your bigger machines.</td>
+                <td class="text-right"><input type="button" value="Add to cart" class="btn btn-primary add-to-cart-btn" id="large-widget" /></td>
+              </tr>
+              <tr>
+                <td>Standard Widget</td>
+                <td>A widget for your most common machines.</td>
+                <td class="text-right"><input type="button" value="Add to cart" class="btn btn-primary add-to-cart-btn" id="standard-widget" /></td>
+              </tr>
+              <tr>
+                <td>Small Widget</td>
+                <td>A widget for your smaller machines.</td>
+                <td class="text-right"><input type="button" value="Add to cart" class="btn btn-primary add-to-cart-btn" id="small-widget" /></td>
+              </tr>
+              <tr>
+                <td>Tiny Widget</td>
+                <td>A widget for your smallest machines.</td>
+                <td class="text-right"><input type="button" value="Add to cart" class="btn btn-primary add-to-cart-btn" id="tiny-widget" /></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm text-right">
+          <input type="submit" class="btn btn-secondary" value="View Cart" />
+        </div>
       </div>
     </div>
     <?php require 'script_include.php' ?>
