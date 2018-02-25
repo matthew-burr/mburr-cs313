fc694034385d67c692d1643a09854739cf25d3ae
Matthew Burr <matt.d.burr@gmail.com>
2018-02-24 21:30:52 -0600

    Fixed layout issue

--- web/recipebox/welcome.php
+++ web/recipebox/welcome.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="stylesheets/main.css" rel="stylesheet" type="text/css" />
   </head>
-  <body class="containerfluid">
+  <body class="container-fluid">
     <div class="row" id="recipeBoxHeader">
       <div class="col-sm">
         <span class="jumbo white shadowed">Recipe Box</span>
